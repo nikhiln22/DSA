ea0479aa699538f348a543f1cda1649b4177bb89
nikhil <nikhilnhegde@gmail.com>
2024-10-31 22:10:36 +0530

    added new problems
    git commit -m "added new problems"

--- dsa week3.js/tree.js
+++ dsa week3.js/tree.js
@@ -628,7 +628,7 @@
 //         let node = new Node(value);
 //         if (this.root === null) {
 //             this.root = node;
-//         } else {
+//         }
 //             this.insertNode(this.root, node);
 //         }
 //     }
@@ -787,6 +787,35 @@ const tree = new Ntree();
 
 tree.insert(10, 8);
 
-console.log(JSON.stringify(tree,null,2));
+console.log(JSON.stringify(tree, null, 2));
 
-// 
+// building an tree from an sorted array
+
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.right = right;
+        this.left = left;
+    }
+}
+
+function sortedArrayToBst(nums) {
+    function BuildBst(left, right) {
+        if (left > right) {
+            return null;
+        }
+
+        let mid = Math.floor((left + right) / 2);
+        let node = new Node(nums[mid]);
+
+        node.left = BuildBst(left, mid - 1);
+        node.right = BuildBst(mid + 1, right);
+
+        return node;
+    }
+
+    return BuildBst(0, nums.length - 1);
+}
+
+
+console.log(sortedArrayToBst())
